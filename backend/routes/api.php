d612d240b03fc349de055785971d0a60681684cc
chudinhtao <chudinhtao123@gmail.com>
2025-06-11 18:53:12 +0700

    UI UX ngon r

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -33,34 +33,12 @@ use App\Http\Controllers\ThongkeController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 
 Route::post('/upload', [UploadController::class, 'upload']);
-
-
-
-
-Route::apiResource('product_types', ProductTypesController::class);
-Route::apiResource('products', ProductController::class);
-// Route::apiResource('brands', BrandsController::class);
-
-
-//Quản lý đơn hàng
-//Admin (tất cả đơn hàng) - ĐẶT TRƯỚC routes có tham số
-// Route::get('/admin/orders', [OrderAdminController::class, 'index']);
-// Route::put('/admin/orders/{id}/status', [OrderAdminController::class, 'updateStatus']);
-
-//Client (đơn hàng theo userID)
-Route::get('/orders', [OrderController::class, 'index']);
-
-
-
 // //Register
 Route::post('/register', [RegisterController::class, 'register']);
 
+// Login+Me+refresh token nhé ae
 Route::post('login', [AuthController::class, 'login']);
 Route::middleware(['jwt.auth'])->group(function () {
     Route::get('user', [AuthController::class, 'me']);
@@ -68,34 +46,32 @@ Route::middleware(['jwt.auth'])->group(function () {
     Route::post('refresh', [AuthController::class, 'refresh']);
 });
 
-//Giỏ hàng + hien thi Tạo
+//Users
 Route::middleware('jwt.auth')->group(function () {
-    //products
-
-Route::apiResource('products', ProductController::class);
-Route::apiResource('brands', App\Http\Controllers\Api\BrandsController::class);
-Route::apiResource('products_types', ProductTypesController::class);
-
-//
+//đơn hàng client
     Route::get('/orders/user', [OrderController::class, 'getOrderByUser']);
-    Route::get('/orders/{id}', [OrderController::class, 'getOrderDetailByOrderId']);
-    //Hủy đơn hàng 
+    Route::get('/orders/{id}', [OrderController::class, 'getOrderDetailByOrderId']); 
     Route::put('/orders/{id}', [OrderController::class, 'updateStatus']);
+//Get danh sách sản phẩm
     Route::get('/products_client', [Home_client::class, 'getByLoai']);
     Route::get('/products_mouse', [Home_client::class, 'getAccessory']);
     Route::get('/laptops/{id}', [Home_client::class, 'getLaptopById']);
     Route::get('/accessory/{id}', [Home_client::class, 'getAccessoryById']);
+//Thao tác với giỏ hàng
     Route::get('/cart', [CartController::class, 'getCartByUser']);
     Route::post('/cart/add', [CartController::class, 'addToCart']);
     Route::put('/cart/{cartId}', [CartController::class, 'updateCart']);
     Route::delete('/cart/{cartId}', [CartController::class, 'deleteCart']);
     Route::post('/buy', [Payment_OrderController::class, 'store']);
-
+//Cập nhật thông tin cá nhân
     Route::put('userClient', [AuthController::class, 'updateMe']);
 });
 
-Route::middleware('jwt.auth')->group(function () {
-//Users
+
+//admin
+
+Route::middleware(['jwt.auth','role:admin'])->group(function () {
+//Quản lý người dùng
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/users/{id}', [UserController::class, 'show']);
     Route::post('/users', [UserController::class, 'store']);
@@ -107,6 +83,8 @@ Route::middleware('jwt.auth')->group(function () {
     Route::get('product_types/{id}', [ProductTypesController::class, 'show']);
     Route::put('product_types/{id}', [ProductTypesController::class, 'update']);
     Route::delete('product_types/{id}', [ProductTypesController::class, 'destroy']);
+//Sản Phẩm
+    Route::apiResource('products', ProductController::class);
 //Brands
     Route::get('brands', [BrandsController::class, 'index']);
     Route::post('brands', [BrandsController::class, 'store']);
